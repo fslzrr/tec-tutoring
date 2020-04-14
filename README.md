6adef0f09b4cd509b0733ac0f5e87adf54eafd19
Fernando Salazar <fslzrr@gmail.com>
2020-04-14 17:58:24 -0500

    initial arquitecture

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# tec-tutoring
+# Tec Tutoring
+
+This project is a progressive web app in react.js with serverless functionalities using firebase services.
+
+### Development
+
+#### Global Dependencies
+
+1. Node [download](https://nodejs.org/en/download/)
+2. Yarn [download](https://classic.yarnpkg.com/en/docs/install)
+3. Firebase tools [download](https://firebase.google.com/docs/cli)
+
+#### Local
+
+1. Install firebase functions dependencies `cd functions && yarn install`.
+2. Build firebase functions `yarn build && cd ..`.
+3. Emulate firebase functions locally `firebase emulators:start --only functions`.
+4. Open new terminal tab.
+5. Add .env file.
+6. Install react app dependencies `yarn install`.
+7. Start react development server `yarn start`.
+
+#### Deployment
+
+1. Build react app `yarn build`.
+2. Deploy firebase functions and hosting `firebase deploy`.
---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import ThemeContext from "./theme/themeContext";
+import styles from "./App.module.scss";
+import Header from "./core/Header";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+type PageType = {
+  to: (page: string, title?: string) => void;
+  toggleTheme: () => void;
+};
+
+const pages: { [key: string]: React.ComponentType<PageType> } = {
+  Login,
+  Signup,
+};
+
+const titles: { [key: string]: string } = {
+  Login: "Welcome to Tec Tutoring",
+  Signup: "Create a new account",
+};
+
+const App: React.FunctionComponent<{}> = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [page, setPage] = useState("Login");
+  const [title, setTitle] = useState("Welcome to Tec Tutoring");
+
+  const themeClass = (
+    styles: { readonly [key: string]: string },
+    theme: string
+  ) => `${styles[`theme-${theme}`]}`;
+
+  const to = (page: string, title?: string) => {
+    setPage(page);
+    setTitle(title === undefined ? titles[page] : title);
+  };
+
+  const toggleTheme = () => {
+    if (theme === "light") setTheme("dark");
+    else setTheme("light");
+  };
+
+  const Page = pages[page];
+
+  return (
+    <ThemeContext.Provider value={{ theme, themeClass }}>
+      <div className={`${styles.App} ${themeClass(styles, theme)}`}>
+        <Header title={title}></Header>
+        <div className={styles.PageContainer}>
+          <Page to={to} toggleTheme={toggleTheme}></Page>
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export type { PageType };
+export default App;
