6cd5b14814ee03df5f16c5a3701935f54d48123a
Aaron Garcia <aarongarciag97@gmail.com>
2020-05-18 03:56:28 -0500

    Added trigger that saves user to firestore.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -8,3 +11,29 @@ export const helloWorld = functions.https.onRequest(
     response.send("Hello from Firebase!");
   }
 );
+
+
+exports.createProfile = functions.auth.user().onCreate((user, context) => {
+  const userEmail = user.email
+  // defaults to student
+  var userType = "student"
+  // defaults to ms since epoch
+  var userDisplayName = Date.now().toString()
+
+  if (userEmail && userEmail.charAt(0).toUpperCase() == 'L') {
+    userType = "professor"
+    userDisplayName = userEmail.split('@')[0]
+  }
+
+  return admin
+    .firestore()
+    .collection('userProfiles')
+    .doc(user.uid)
+    .set({
+      email: user.email,
+      displayName: userDisplayName,
+      type: userType
+    }).then(writeResult => {
+      console.log("Created new user with display name: \"" + userDisplayName + "\"!")
+    });
+});
