ae8ac20921b71d1dbd2c9e11d9969bee94f58fe5
Aaron Garcia <aarongarciag97@gmail.com>
2020-05-25 03:05:51 -0500

    Professor can now filter by course area.

--- src/pages/ProfessorHome.tsx
+++ src/pages/ProfessorHome.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { PageType } from "."
 import Button from "../core/Button"
@@ -9,8 +9,11 @@ import { Session } from "../models/Session"
 import { startSession } from "../helpers/sessions"
 
 function getQuailfyingSessionsForCurrentUser(area: string) {
-  return SessionCollection.where('area', '==', area)
-    .where('pending', '==', true)
+  return SessionCollection.where('pending', '==', true)
+}
+
+function getUniqueAreasFromSessions(sessions: Session[] | undefined) {
+  return sessions ? Array.from(new Set(sessions.map(s => s.area))).sort() : ['']
 }
 
 const ProfessorHome: React.FunctionComponent<PageType> = () => {
@@ -18,13 +21,35 @@ const ProfessorHome: React.FunctionComponent<PageType> = () => {
   const [values] = useCollectionData<Session>(
     getQuailfyingSessionsForCurrentUser(user?.area || ''), { idField: 'id' })
 
+  const areas: Array<string> = getUniqueAreasFromSessions(values)
+  const [area, setArea] = useState(areas[0] ? areas[0] : 'none')
+
   return (
     <div>
-      {user && values && values.map(s =>
-        <li key={s.id}>{s.student}
-          <Button onClick={() => startSession(s.id, user.uid)}>Aceptar</Button>
-        </li>
-      )}
+      <div>
+        <select
+          id="dropdown"
+          value={area}
+          onChange={event => setArea(event.target.value)}>
+          <option key='none'>none</option>
+          {user && areas && areas.map(a =>
+            <option key={a}>
+              {a}
+            </option>
+          )}
+        </select>
+      </div>
+      <div>
+        {user && values && values.map(s =>
+          // TODO: Fix: When first loaded it doesn't display any sessions, even though the select has a value,
+          // it only displays after selecting something.
+          // HOTFIX: Added none as default selector.
+          s.area === area &&
+          <li key={s.id}>{s.student}
+            <Button onClick={() => startSession(s.id, user.uid)}>Aceptar</Button>
+          </li>
+        )}
+      </div>
     </div>
   );
 };
